4e2c27e45a6d40a15abac455b6c5e6b9d0868323
Adharsh M <dev@adharsh.in>
2023-06-23 19:54:22 +0700

    chore: merged user storage.

--- pkg/http/handlers/register.go
+++ pkg/http/handlers/register.go
@@ -2,7 +2,7 @@ package handlers
 
 import "github.com/adharshmk96/stk"
 
-func RegisterHandler(ctx *stk.Context) {
+func RegisterHandler(ctx stk.Context) {
 	ctx.Status(200).JSONResponse(stk.Map{
 		"message": "Hello World",
 	})
