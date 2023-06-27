aa631e8a9ea87ed14e11ef74d72652f5301307d8
Adharsh M <dev@adharsh.in>
2023-06-28 00:13:19 +0700

    bump version to v0.1.0

--- cmd/root.go
+++ cmd/root.go
@@ -9,7 +9,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "v0.0.3"
+var version = "v0.1.0"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
