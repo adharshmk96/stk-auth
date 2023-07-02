bfa59d5ca181eeccf1d202ef9c4f7a740c686ed8
Adharsh M <dev@adharsh.in>
2023-07-03 01:49:17 +0700

    feat: add user via cli.

--- server/routing.go
+++ server/routing.go
@@ -2,13 +2,18 @@ package server
 
 import (
 	"github.com/adharshmk96/stk-auth/pkg/http/handlers"
+	"github.com/adharshmk96/stk-auth/pkg/infra/constants"
 	"github.com/adharshmk96/stk-auth/pkg/services"
 	"github.com/adharshmk96/stk-auth/pkg/storage/sqlite"
 	"github.com/adharshmk96/stk/gsk"
+	"github.com/adharshmk96/stk/pkg/db"
+	"github.com/spf13/viper"
 )
 
 func setupRoutes(server *gsk.Server) {
-	userStorage := sqlite.NewAccountStorage()
+	connection := db.GetSqliteConnection(viper.GetString(constants.ENV_SQLITE_FILE))
+
+	userStorage := sqlite.NewAccountStorage(connection)
 	userService := services.NewAccountService(userStorage)
 	userHandler := handlers.NewAccountHandler(userService)
 
@@ -16,6 +21,8 @@ func setupRoutes(server *gsk.Server) {
 
 	server.Post("/api/auth/session/login", userHandler.LoginUserSession)
 	server.Post("/api/auth/session/login/token", userHandler.LoginUserSessionToken)
+
+	server.Post("/api/oidc/login", userHandler.LoginUserSessionToken)
 
 	server.Get("/api/auth/session/user", userHandler.GetSessionUser)
 	server.Get("/api/auth/session/user/token", userHandler.GetSessionTokenUser)
